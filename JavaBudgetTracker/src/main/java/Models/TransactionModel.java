867ae4a0443bab959588dc26947fda02130015ac
KentCounts <KentCounts00@gmail.com>
2026-05-13 17:47:20 -0500

    outline for transaction modeling

--- JavaBudgetTracker/src/main/java/Models/TransactionModel.java
+++ JavaBudgetTracker/src/main/java/Models/TransactionModel.java
@@ -8,6 +8,54 @@ package Models;
  *
  * @author Kent
  */
+
+// LocalDate
+// TransactionType enum TODO: Build
+
 public class TransactionModel {
-    
+
+    // unique transaction id
+    // transaction amount
+    // category name
+    // example:
+    //     Food
+    //     Rent
+    //     Entertainment
+    // transaction description/notes
+    // transaction date
+    // transaction type
+    // INCOME or EXPENSE
+
+    // CONSTRUCTORS
+    // default constructor
+    // full constructor
+    // initialize all fields
+
+    // GETTERS
+    // get id
+    // get amount
+    // get category
+    // get description
+    // get date
+    // get transaction type
+
+    // SETTERS
+    // set id
+    // set amount
+    // set category
+    // set description
+    // set date
+    // set transaction type
+
+    // OUTPUT
+    // function: toString()
+    // return formatted transaction string
+
+    // FUTURE IMPROVEMENTS
+    // tags/labels
+    // recurring transaction flag
+    // payment method
+    // merchant/vendor name
+    // receipt attachment support
+    // transaction validation
 }
